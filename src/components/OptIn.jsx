324f011c4966bea21f4c822275a9198503b036eb
Charles Burgess <consulting@cvburgess.com>
2017-07-06 22:08:46 -0400

    style the opt-in screen

--- src/components/OptIn.jsx
+++ src/components/OptIn.jsx
@@ -2,18 +2,40 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const primaryColor = '#185A9D';
+const secondaryColor = '#43CEA2';
+const dangerColor = '#F15E14';
+const warningColor = '#F1BE14';
+
 const Container = styled.div``;
-const Input = styled.input``;
-const Button = styled.button``;
+const CheckboxButton = styled.button`
+  color: #000;
+  font-size: 20px;
+  background: ${props => props.hasOptedIn ? secondaryColor : dangerColor};
+  padding: 10px 15px 10px 15px;
+  text-decoration: none;
+  border: none;
+  max-height: 50px;
+  margin-right: 10px;
+`;
+const Button = styled.button`
+  color: #000;
+  font-size: 20px;
+  background: #fff;
+  padding: 10px 15px 10px 15px;
+  text-decoration: none;
+  border: none;
+  max-height: 50px;
+`;
 
 class OptIn extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { hasOptedIn: false };
+    this.state = { hasOptedIn: true };
   }
 
-  toggleOptIn = ({ target }) => this.setState({ hasOptedIn: target.checked });
+  toggleOptIn = () => this.setState((state, props) => ({ hasOptedIn: !state.hasOptedIn }));
 
   sumbitOptIn = () => this.props.onSubmit(this.state.hasOptedIn);
 
@@ -21,11 +43,10 @@ class OptIn extends Component {
     const { hasOptedIn } = this.state;
     return (
       <Container>
-        <Input
-          onChange={this.toggleOptIn}
-          type="checkbox"
-          value={hasOptedIn}>
-        </Input>
+        <CheckboxButton
+          onClick={this.toggleOptIn}
+          hasOptedIn={hasOptedIn}
+        >{hasOptedIn ? 'Spitfire can send me emails' : 'Do not send me emails'}</CheckboxButton>
         <Button onClick={this.sumbitOptIn}>{'>'}</Button>
       </Container>
     );
